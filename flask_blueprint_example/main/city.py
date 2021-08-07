17093a43601b2982ea24854608a8603748c077c0
DESKTOP-I6BO71Q\Hellllo <must1080@naver.com>
2021-08-07 20:48:07 +0900

    test

--- flask_blueprint_example/main/city.py
+++ flask_blueprint_example/main/city.py
@@ -1,8 +1,7 @@
 from flask import Blueprint
 #   - name은 블루프린트의 이름을 나타낸다. 
-
 #   - import_name은 블루프린트 패키지의 이름이다. 보통 __name__으로 많이 쓰인다. 이는 블루프린트의 root_path의 위치를 찾는데 도움을 준다.
-# Blueprint( 이름, 모듈명, URL 프리픽스(url_prefix) )
+#   - Blueprint( 이름, 모듈명, URL 프리픽스(url_prefix) )
 blue_city = Blueprint("city", import_name = __name__, url_prefix="/city")
 
 @blue_city.rout("/seoul")
